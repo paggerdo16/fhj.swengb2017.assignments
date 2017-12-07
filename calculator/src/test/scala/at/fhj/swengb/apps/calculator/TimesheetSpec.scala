1762967272396901ecd12b71524b0776daf8ca98
kossrocks <mathias.puetzl@edu.fh-joanneum.at>
2017-12-07 15:25:55 +0100

    Configured TimesheetSpec

---
+++ calculator/src/test/scala/at/fhj/swengb/apps/calculator/TimesheetSpec.scala
@@ -0,0 +1,55 @@
+package at.fhj.swengb.apps.calculator
+
+import java.nio.file.{Files, Paths}
+import java.util
+
+import org.scalatest.WordSpecLike
+
+import scala.collection.JavaConverters._
+
+class TimesheetSpec extends WordSpecLike {
+
+
+  val file = "C:\\Workspace\\fhj.swengb2017.assignments\\calculator\\timesheet-calculator.adoc"
+  val new_file = Paths.get(file)
+
+  println(Files.readAllLines(new_file))
+
+  val timesheet = """== Time expenditure: Calculator assignment
+                    |
+                    |
+                    |[cols="1,1,1,4", options="header"]
+                    |.Time expenditure
+                    ||===
+                    || Date
+                    || Hours (planned)
+                    || Hours (spend)
+                    || Description
+                    |
+                    || 04.12.17
+                    || 2
+                    || 1
+                    || Create Buttons
+                    |
+                    || 04.12.17
+                    || 1
+                    || 1.5
+                    || Read this and that
+                    |
+                    || 11.07.17
+                    || --
+                    || --
+                    || --
+                    |
+                    |
+                    |
+                    ||===
+                    |""".stripMargin
+
+  "Timetable" should {
+    "be the same as expected" in {
+      assert(println(Files.readAllLines(new_file)) != timesheet)
+    }
+
+  }
+}
